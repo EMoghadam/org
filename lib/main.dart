54e02d802921c9274304904b8cb959d6f99aec48
Ehsan <emm.programmer@gmail.com>
2022-05-01 03:23:11 +0430

    change int to String for parameter in methods playsound

--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,14 @@ class org extends StatefulWidget {
 }
 
 class _orgState extends State<org> {
-  late int voisNumber;
+  late String voisNumber;
   final AudioCache player = AudioCache();
-  void playsound(int number) {
+  void playsound(String number) {
     player.play("key$number.mp3");
     print("assets/key$number.mp3");
   }
 
-  Widget click({required Color color, required int voisNumber}) {
+  Widget click({required Color color, required String voisNumber}) {
     return Expanded(
       flex: 1,
       child: InkWell(
@@ -43,16 +43,16 @@ class _orgState extends State<org> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              click(color: Colors.red,voisNumber: 01),
-              click(color: Colors.deepOrange,voisNumber: 02),
-              click(color: Colors.orange,voisNumber: 03),
-              click(color: Colors.yellowAccent,voisNumber: 04),
-              click(color: Colors.green,voisNumber: 05),
-              click(color: Colors.lightGreen,voisNumber: 06),
-              click(color: Colors.lightBlueAccent,voisNumber: 07),
-              click(color: Colors.blue,voisNumber: 08),
-              click(color: Colors.indigo,voisNumber: 09),
-              click(color: Colors.deepPurple,voisNumber: 10),
+              click(color: Colors.red,voisNumber: "01"),
+              click(color: Colors.deepOrange,voisNumber: "02"),
+              click(color: Colors.orange,voisNumber: "03"),
+              click(color: Colors.yellowAccent,voisNumber: "04"),
+              click(color: Colors.green,voisNumber: "05"),
+              click(color: Colors.lightGreen,voisNumber: "06"),
+              click(color: Colors.lightBlueAccent,voisNumber: "07"),
+              click(color: Colors.blue,voisNumber: "08"),
+              click(color: Colors.indigo,voisNumber: "09"),
+              click(color: Colors.deepPurple,voisNumber: "10"),
             ],
           ),
         ),
